c35b3ff0754c9b82647ad9ca1dfcd545e152c1d9
Rizanriz <rizanr7777@gmail.com>
2024-05-07 09:43:49 +0530

    ongoing commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server")
+
+const MPServer = jsonServer.create()
+
+const middleware = jsonServer.defaults()
+
+const route = jsonServer.router("db.json")
+
+const PORT = 3000 || process.env.PORT
+
+
+MPServer.use(middleware)
+MPServer.use(route)
+MPServer.listen(PORT,()=>{
+    console.log(`media player server started at port ${PORT} and waitiong for client request`);
+})
